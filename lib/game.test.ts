045db93e2148e079dfc286f7f6c06346d30fde69
Brennan Colberg <brennan.colberg@gmail.com>
2021-12-02 08:56:34 -0800

    test countNeighbors

--- lib/game.test.ts
+++ lib/game.test.ts
@@ -1,4 +1,4 @@
-import { startGame } from "./game"
+import { startGame, countNeighbors } from "./game"
 describe(startGame, () => {
   it("works with zeroes", () => {
     expect(startGame({ width: 0, height: 0 }).state.length).toBe(0)
@@ -11,5 +11,58 @@ describe(startGame, () => {
   })
   it("works when the height is larger", () => {
     expect(startGame({ width: 2, height: 3 }).state.length).toBe(6)
+  })
+})
+
+describe(countNeighbors, () => {
+  const state = [
+    "001111", // long comment so prettier keeps it in multiple lines
+    "101010",
+    "011100",
+    "101001",
+    "011001",
+    "110000",
+  ].flatMap((row) => [...row].map((char) => char === "1"))
+  const game = {
+    width: 6,
+    height: 6,
+    state,
+  }
+
+  it("counts corners properly", () => {
+    expect(countNeighbors(game, 0)).toBe(1)
+    expect(countNeighbors(game, 5)).toBe(2)
+    expect(countNeighbors(game, 30)).toBe(2)
+    expect(countNeighbors(game, 35)).toBe(1)
+  })
+  it("counts top properly", () => {
+    expect(countNeighbors(game, 1)).toBe(3)
+    expect(countNeighbors(game, 2)).toBe(2)
+    expect(countNeighbors(game, 3)).toBe(4)
+    expect(countNeighbors(game, 4)).toBe(3)
+  })
+  it("counts bottom properly", () => {
+    expect(countNeighbors(game, 31)).toBe(3)
+    expect(countNeighbors(game, 32)).toBe(3)
+    expect(countNeighbors(game, 33)).toBe(1)
+    expect(countNeighbors(game, 34)).toBe(1)
+  })
+  it("counts sides properly", () => {
+    // left
+    expect(countNeighbors(game, 6)).toBe(1)
+    expect(countNeighbors(game, 12)).toBe(3)
+    expect(countNeighbors(game, 18)).toBe(2)
+    expect(countNeighbors(game, 24)).toBe(3)
+    // right
+    expect(countNeighbors(game, 7)).toBe(3)
+    expect(countNeighbors(game, 13)).toBe(2)
+    expect(countNeighbors(game, 19)).toBe(1)
+    expect(countNeighbors(game, 25)).toBe(1)
+  })
+  it("counts middle properly", () => {
+    expect(countNeighbors(game, 14)).toBe(4)
+    expect(countNeighbors(game, 15)).toBe(4)
+    expect(countNeighbors(game, 20)).toBe(5)
+    expect(countNeighbors(game, 21)).toBe(4)
   })
 })
--- lib/game.ts
+++ lib/game.ts
@@ -12,20 +12,7 @@ export const startGame = ({ width, height }: Dimensions): Game => ({
   state: Array(width * height).fill(false),
 })
 
-export const tickGame = (game: Game): Game => ({
-  ...game,
-  state: game.state.map((alive, index) => {
-    const neighbors = countNeighbors(game, index)
-    // if it's alive with ≤1 or ≥4 neighbors, it dies
-    if (alive && (neighbors <= 1 || neighbors >= 4)) return false
-    // if it's empty with 3 neighbors, it comes to life
-    if (!alive && neighbors === 3) return true
-    // otherwise, it stays the same (alive or dead)
-    return alive
-  }),
-})
-
-const countNeighbors = (game: Game, index: number): number =>
+export const countNeighbors = (game: Game, index: number): number =>
   [
     // top
     game.state[index - game.width - 1],
@@ -39,3 +26,16 @@ const countNeighbors = (game: Game, index: number): number =>
     game.state[index + game.width],
     game.state[index + game.width + 1],
   ].reduce((result, neighbor) => (neighbor ? result + 1 : result), 0)
+
+export const tickGame = (game: Game): Game => ({
+  ...game,
+  state: game.state.map((alive, index) => {
+    const neighbors = countNeighbors(game, index)
+    // if it's alive with ≤1 or ≥4 neighbors, it dies
+    if (alive && (neighbors <= 1 || neighbors >= 4)) return false
+    // if it's empty with 3 neighbors, it comes to life
+    if (!alive && neighbors === 3) return true
+    // otherwise, it stays the same (alive or dead)
+    return alive
+  }),
+})
