78bc05e3bd1b9729fd7f40d8a89e46f251bc9e26
Brennan Colberg <brennan.colberg@gmail.com>
2021-12-02 10:25:19 -0800

    state ticks & renders

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,17 @@
 import type { NextPage } from "next"
+import { useEffect, useState } from "react"
+import { Game, startGame, tickGame } from "../lib/game"
 
 const Home: NextPage = () => {
-  return <p className="text-red-400">placeholder</p>
+  const [game, setGame] = useState<Game>(startGame({ width: 10, height: 10 }))
+
+  useEffect(() => {
+    const tick = () => setGame((game) => tickGame(game))
+    const interval = setInterval(tick, 1000)
+    return () => clearInterval(interval)
+  }, [])
+
+  return <pre>{JSON.stringify(game, undefined, 2)}</pre>
 }
 
 export default Home
