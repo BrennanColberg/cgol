7764c44b1616ce60b80f6064292b682bbb0e1330
Brennan Colberg <brennan.colberg@gmail.com>
2021-12-02 11:51:01 -0800

    switch to canvas (speed, size)

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,37 +1,45 @@
 import type { NextPage } from "next"
-import { useEffect, useState } from "react"
+import { useEffect, useRef, useState } from "react"
 import { Game, startGame, tickGame } from "../lib/game"
-import classNames from "classnames"
 
-const width = 20
-const height = 20
+const width = 256
+const height = 256
 
 const Home: NextPage = () => {
+  const canvasRef = useRef<HTMLCanvasElement>(null)
   const [game, setGame] = useState<Game>(startGame({ width, height }))
 
   useEffect(() => {
     const tick = () => setGame((game) => tickGame(game))
-    const interval = setInterval(tick, 100)
+    const interval = setInterval(tick, 50)
     return () => clearInterval(interval)
   }, [])
+
+  useEffect(() => {
+    const canvas = canvasRef.current?.getContext("2d")
+    if (!canvas) return
+    canvas.clearRect(0, 0, width, height)
+    game.state.forEach((alive, index) => {
+      if (!alive) return
+      canvas.fillRect(index % width, Math.floor(index / width), 1, 1)
+    })
+  }, [canvasRef, game])
 
   return (
     <div
       className="w-screen h-screen flex justify-center items-center"
       onClick={() => setGame(startGame({ width, height }))}
     >
-      <div
-        className="flex flex-row flex-wrap"
-        style={{ width: "100vmin", height: "100vmin" }}
-      >
-        {game.state.map((alive, i) => (
-          <div
-            key={i}
-            className={classNames({ "bg-black": alive })}
-            style={{ width: `${100 / width}%`, height: `${100 / height}%` }}
-          />
-        ))}
-      </div>
+      <canvas
+        style={{
+          width: "100vmin",
+          height: "100vmin",
+          imageRendering: "crisp-edges",
+        }}
+        width={width}
+        height={height}
+        ref={canvasRef}
+      />
     </div>
   )
 }
