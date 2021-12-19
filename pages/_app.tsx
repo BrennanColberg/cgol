872ae852806f10fd3bd6b8db71cdc756b35e0515
Brennan Colberg <brennan.colberg@gmail.com>
2021-12-19 00:22:21 -0800

    comprehensive meta tags (incl image)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,11 +7,31 @@ function MyApp({ Component, pageProps }: AppProps) {
     <>
       <Head>
         <title>Conway&apos;s Game of Life</title>
+        <meta name="title" content="Conway's Game of Life" />
         <meta
           name="description"
           content="A full-screen in-browser simulation"
         />
-        <meta name="author" content="Brennan Colberg" />
+
+        {/* Open Graph / Facebook */}
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://cgol.app/" />
+        <meta property="og:title" content="Conway's Game of Life" />
+        <meta
+          property="og:description"
+          content="A full-screen in-browser simulation"
+        />
+        <meta property="og:image" content="https://cgol.app/preview.png" />
+
+        {/* Twitter */}
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:url" content="https://cgol.app/" />
+        <meta property="twitter:title" content="Conway's Game of Life" />
+        <meta
+          property="twitter:description"
+          content="A full-screen in-browser simulation"
+        />
+        <meta property="twitter:image" content="https://cgol.app/preview.png" />
       </Head>
       <Component {...pageProps} />
     </>
